19f8f18f507868e69d06bc36f5b071811483087e
Harin-Mabao <mixharinmabao@gmail.com>
2024-07-04 01:04:58 +0700

    Update app.js Rev00 First issued

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-// write code here
+let user = prompt("username");
+let pass = prompt("password");
+
+if(!user && !pass ){alert("username and password is required")}
+else if(!user){alert("username is required")}
+ else if(!pass){alert("password is required")}
+ else if(user == "admin" && pass == "1234" || user == "john" && pass == "qwerty" ){
+    alert(`Hello ${user}`)}
+else{alert("invalid username or password");}
+
+
+console.log(`username : ${user}`)
+console.log(`password : ${pass}`)
